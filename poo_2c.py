6e182786eceffcdb85b1ce9c19f8cba7b055a1c8
AMCC107 <mauriciocc11@hotmail.com>
2025-01-15 11:05:05 -0600

    Ajustes en atacar

--- poo_2c.py
+++ poo_2c.py
@@ -32,16 +32,18 @@ class Personaje:
     def morir(self):
         self.vida=0
         print(self.nombre, "ha muerto")
-    def dañar(self,enemigo):
-        return self.fuerza-enemigo.defensa
-    def atacar(self,enemigo):
-        daño=self.dañar(enemigo)
-        enemigo.vida=enemigo.vida-daño
-        print(self.nombre, "ha realizado",daño,"puntos de daño a",enemigo.nombre)
-        print("Vida de ",enemigo.nombre,"es",enemigo.vida)
+    def dañar(self, enemigo):
+        daño = self.fuerza - enemigo.defensa
+        return max(daño, 0)
+    def atacar(self, enemigo):
+        daño = self.dañar(enemigo)
+        enemigo.vida -= daño
+        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        print("Vida de", enemigo.nombre, "es", enemigo.vida)
+
 #Constructor
 #Variable del constructor 
-mi_personaje = Personaje("EsteBandido", 100,50,45,100)
+mi_personaje = Personaje("EsteBandido", 140,50,45,100)
 mi_enemigo=Personaje("Ángel",70,100,40,100)
 mi_personaje.imprimir_atributos() 
 mi_personaje.atacar(mi_enemigo)
