e10ebe32e3d793416dc189c61087f27ff07c0199
AMCC107 <mauriciocc11@hotmail.com>
2025-01-15 12:39:01 -0600

    Simulando encapsulación

--- poo_2c.py
+++ poo_2c.py
@@ -1,10 +1,10 @@
 class Personaje: 
     def __init__(self, nombre, fuerza, inteligencia, defensa,vida):
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.defensa = defensa
-        self.vida = vida
+        self.__nombre = nombre
+        self.__fuerza = fuerza
+        self.__inteligencia = inteligencia
+        self.__defensa = defensa
+        self.__vida = vida
 #Pregunta de examen ¿Qué es self en programación? R= Es una referencia al mismo objeto
 #¿Qué es el método init? R= Constructor  que inicializa atributos de un objeto
 #¿Por qué se usa dobre guión bajo para init? R= Dunder. Método especial de Python, método mágico, predefinido por python
@@ -17,47 +17,60 @@ class Personaje:
     #defensa = 0
     #vida = 0
     def imprimir_atributos(self):
-        print(self.nombre)
-        print("-Fuerza:",self.fuerza)
-        print("-Inteligencia:", self.inteligencia)
-        print("-Defensa:", self.defensa)
-        print("-Vida:", self.vida)
+        print(self.__nombre)
+        print("-Fuerza:",self.__fuerza)
+        print("-Inteligencia:", self.__inteligencia)
+        print("-Defensa:", self.__defensa)
+        print("-Vida:", self.__vida)
     def subir_nivel(self,fuerza, inteligencia, defensa):
-        self.fuerza=self.fuerza+fuerza
-        #self.fuerza+=fuerza
-        self.inteligencia=self.inteligencia+inteligencia
-        self.defensa=self.defensa+defensa
+        self.__fuerza=self.__fuerza+fuerza
+        #self.__fuerza+=fuerza
+        self.__inteligencia=self.__inteligencia+inteligencia
+        self.__defensa=self.__defensa+defensa
     def esta_vivo(self):
-        return self.vida > 0
+        return self.__vida > 0
     def morir(self):
-        self.vida=0
-        print(self.nombre, "ha muerto")
+        self.__vida=0
+        print(self.__nombre, "ha muerto")
     def dañar(self, enemigo):
-        daño = self.fuerza - enemigo.defensa
+        daño = self.__fuerza - enemigo.__defensa
         return max(daño, 0)
     def atacar(self, enemigo):
         daño = self.dañar(enemigo)
-        enemigo.vida -= daño
-        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
-        print("Vida de", enemigo.nombre, "es", enemigo.vida)
-
+        if daño < 0: daño=0
+        enemigo.__vida -= daño
+        print(self.__nombre, "ha realizado", daño, "puntos de daño a", enemigo.__nombre)
+        print("Vida de", enemigo.__nombre, "es", enemigo.__vida)
+    def get_vida(self):
+        return self.__vida
+    def set_vida(self,vida):
+        self.__vida = vida
+        if self.__vida < 0: self.__vida=0
+        print(self.__nombre, "ha recibido", vida, "puntos de vida")
+        print("Vida de", self.__nombre, "es", self.__vida)
 #Constructor
 #Variable del constructor 
 mi_personaje = Personaje("EsteBandido", 140,50,45,100)
 mi_enemigo=Personaje("Ángel",70,100,40,100)
-mi_personaje.imprimir_atributos() 
-mi_personaje.atacar(mi_enemigo)
+print(mi_personaje.get_vida())
+#mi_personaje.set_vida(-5)
+mi_personaje._Personaje__vida=-50
+print(mi_personaje.get_vida())
+#mi_personaje.imprimir_atributos() 
+#mi_personaje.atacar(mi_enemigo)
 # mi_personaje.subir_nivel(15,5,10)
 # print("Valores actualizados")
 # mi_personaje.imprimir_atributos()
 #Modificando valores de los atributos
-# mi_personaje.nombre="EstebanDido"
-# mi_personaje.fuerza=300
-# mi_personaje.inteligencia=-2
-# mi_personaje.defensa=30
-# mi_personaje.vida=2
-# print("El nombre de mi personaje es:",mi_personaje.nombre)
-# print("La fuerza de mi personaje es:",mi_personaje.fuerza)
-# print("La inteligencia de mi personaje es:",mi_personaje.inteligencia)
-# print("La defensa de mi personaje es:",mi_personaje.defensa)
-# print("La vida de mi personaje es:",mi_personaje.vida)
+# mi_personaje.__nombre="EstebanDido"
+# mi_personaje.__fuerza=300
+# mi_personaje.__inteligencia=-2
+# mi_personaje.__defensa=30
+# mi_personaje.__vida=2
+# print("El nombre de mi personaje es:",mi_personaje.__nombre)
+# print("La fuerza de mi personaje es:",mi_personaje.__fuerza)
+# print("La inteligencia de mi personaje es:",mi_personaje.__inteligencia)
+# print("La defensa de mi personaje es:",mi_personaje.__defensa)
+# print("La vida de mi personaje es:",mi_personaje.__vida)
+#Getters and setters estudiar para el examen
+
